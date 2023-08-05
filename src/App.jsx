e9d2cb70dc80a00306557b22c09f39bb79f960d5
Mauro Villagra <maurovillagra257@gmail.com>
2023-08-04 21:46:11 -0300

    Termine de añadir la api

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import { Container } from 'react-bootstrap'
+import Formulario from "./componentes/Formulario";
 
 function App() {
 
   return (
     <>
-      <div></div>
+    <Container>
+    <h1 className='my-5 text-center'>Portal de Noticias</h1>
+    <Formulario/>
+    </Container>
     </>
   )
 }
